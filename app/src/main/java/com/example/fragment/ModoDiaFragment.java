53cf9b351188ebef2821a57bc911f1ec52cc96a3
Carlos Dario Flores <cdfn3@hotmail.com>
2019-06-12 16:58:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/fragment/ModoDiaFragment.java
@@ -0,0 +1,30 @@
+package com.example.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ModoDiaFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ModoDiaFragment extends Fragment {
+
+    public ModoDiaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modo_dia, container, false);
+    }
+
+}
